16690e09c19b9e830596f758610ab4ca878db5d8
Will <68452517+willthepillow@users.noreply.github.com>
2021-01-14 01:44:55 +0000

    Update main.py
    this is just a computer science project, its done now.

--- PROJECT/main.py
+++ PROJECT/main.py
@@ -1,56 +1,65 @@
+
 #import other stuff here
-from io import SEEK_END
 import sys
 import random
-from colorama import force
 x = 0
 username = input("What is your username:")
 password = input("""What is your password?
 hint, 123:""")
 
-if password == "123":
+if password ==  "123":
     print("user has been authorized")
+    print ("-" * 50)
+    print ("-" * 18 +" STARTING GAME " + "-" * 17)
+    print ("-" * 50)
+#this checks if the password is correct
 
 elif password != ("123"):
     print("User has not been authorized")
     sys.exit()
+#this runs if the password has not been written correctly, and exits the program
 
+def random_song(fname):
+    lines = open(fname).read().splitlines()
+    return random.choice(lines)
+#this function selects a random line
 
-read = open("songlist.txt", "rt")
-songs = read.readlines()
-songlist = []
+user_score = 0
+post = 1
 
-score = 0
-
-for i in range (length(songs)):
-    songlist.append(songs[i].strip('\n'))
-
-while x == 0:
-    choice = random.choice(songlist)
-    artist, song = choice.split(" ")
-
-songs = songs.split()
-letters = [word[0] for word in songs]
-
-for x in range (0,2):
-    print(artist, "".join(letters))
-    guess = str(input("Guess the song:"))
-    print("\n")
-    if guess == song:
-        if x ==0:
-            score = score + 3
+while post == 1:
+    song_list = random_song("songlist.txt")
+    songname, artist = song_list.split(";")#this splits the songlist.txt file where there is a ";"
+    user_guess = input  ("""Whats the name of the song?
+    hint, it begins with...""" + songname[0] + ":")
+     #this asks the user the songname and gives them a hint
+    if user_guess == songname:
+        user_score = user_score + 1 #if the user is correct, they score a point
+        print ("correct, your score is now: " + str(user_score) + " points")#this prints the user score
+    else:
+        user_guess = input("incorrect, try again:") #if the user gets the first guess wrong, they get another chance
+        if user_guess == songname:
+            user_score = user_score + 1 #if the user guess correctly, they get another point and they get to try another song
+            print ("correct, your score is now: " + str(user_score) + " points") #^^
+        else:
+            print ("incorrect, game over.") #if they get the 2 chances wrong, the loop ends
+            print ("Score = " + str(user_score)) #this prints the user score
+            post == 0
             break
-        if x == 1:
-            score = score + 1
-        break
-print("Your score is", score)
-print("Be ready for the next one")
-print("\n")
+        
 
-leaderboard = open("leaderboard.txt", "rt+")
-leaderboard.write(username + "-" + "{}" .format(score))
+score_1 = str(user_score) 
+
+print("\n") #the \n just creates an empty line to make the program more visualy pleasing to the user/ more aesthetic
+print ("-" * 40)
+print("-" * 15 + "LEADERBOARD" + "-" * 14)
+print ("-" * 40)
+print("\n") #this is just a banner presenting the scoreboard
+
+leaderboard = open("leaderboard.txt", "r+")
+leaderboard.write(username + "-" + '{}'.format(user_score)) #this writes the user's score into the leaderboard.txt file
 leaderboard.close()
-leaderboard.open("leaderboard.txt", "rt+")
-leaderboard_file = leaderboard.readlines()
-print(leaderboard_file)
+leaderboard = open("leaderboard.txt", "r+")
+leaderboard_list = leaderboard.readlines()
+print(leaderboard_list) #this prints the user/ user's score underneath the leaderboard banner
 leaderboard.close()
